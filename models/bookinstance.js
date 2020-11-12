78bbe359adf1fb89fd2c7d7651a0827c18884a3c
JuanG <juangiacosabrk@gmail.com>
2020-11-12 10:44:08 -0300

    BookInstance List page updated to present the due_date field in a more friendly format

--- models/bookinstance.js
+++ models/bookinstance.js
@@ -1,5 +1,7 @@
 var mongoose = require('mongoose');
 
+const { DateTime } = require("luxon");
+
 var Schema = mongoose.Schema;
 
 var BookInstanceSchema = new Schema(
@@ -17,6 +19,12 @@ BookInstanceSchema
 .get(function () {
   return '/catalog/bookinstance/' + this._id;
 });
+
+//Virtual for formated due date in  DATE_MED format in English: Oct 6th, 2020.
+BookInstanceSchema.virtual('due_back_formatted')
+    .get(function(){
+      return DateTime.fromJSDate(this.due_back).toLocaleString(DateTime.DATE_MED);
+    })
 
 //Export model
 module.exports = mongoose.model('BookInstance', BookInstanceSchema);
