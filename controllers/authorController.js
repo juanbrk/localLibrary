5ae0532c28c3eff60ca997929776c2b4e0679760
JuanG <juangiacosabrk@gmail.com>
2020-11-13 10:40:39 -0300

    Implemented Create Author page for creating a new author.

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -2,6 +2,8 @@ var Author = require('../models/author');
 var Book = require('../models/book');
 var async = require('async');
 
+var { body, validationResult  } = require('express-validator')
+
 // Display list of all Authors.
 exports.author_list = function(req, res, next) {
 
@@ -41,14 +43,81 @@ exports.author_detail = function(req, res, next) {
 };
 
 // Display Author create form on GET.
-exports.author_create_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: Author create GET');
+exports.author_create_get = function(req, res, next) {       
+    res.render('author_form', { title: 'Create Author'});
 };
 
 // Handle Author create on POST.
-exports.author_create_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: Author create POST');
-};
+exports.author_create_post = [
+
+    // Validate and sanitise fields.
+    /**
+     * We can daisy chain validators, using withMessage() to specify the error 
+     * message to display if the previous validation method fails.
+     * 
+     * We can use the optional() function to run a subsequent validation only 
+     * if a field has been entered
+     * 
+     * checkFalsy flag means that we'll accept either an empty string or null 
+     * as an empty value
+     * 
+     * Parameters are received from the request as strings. We can use toDate() (or toBoolean()) to cast 
+     * these to the proper JavaScript types 
+     * 
+     * The validation and sanitization chains are middleware that should be passed to the Express route 
+     * handler (we do this indirectly, via the controller). When the middleware runs, each 
+     * validator/sanitizer is run in the order specified
+     * 
+     */
+    body('first_name')
+        .trim()
+        .isLength({ min: 1 })
+        .escape().withMessage('First name must be specified.')
+        .isAlphanumeric().withMessage('First name has non-alphanumeric characters.'),
+    body('family_name')
+        .trim()
+        .isLength({ min: 1 })
+        .escape().withMessage('Family name must be specified.')
+        .isAlphanumeric().withMessage('Family name has non-alphanumeric characters.'),
+    body('date_of_birth', 'Invalid date of birth')
+        .optional({ checkFalsy: true })
+        .isISO8601()
+        .toDate(),
+    body('date_of_death', 'Invalid date of death')
+        .optional({ checkFalsy: true })
+        .isISO8601()
+        .toDate(),
+
+    // Process request after validation and sanitization.
+    (req, res, next) => {
+
+        // Extract the validation errors from a request.
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            // There are errors. Render form again with sanitized values/errors messages.
+            res.render('author_form', { title: 'Create Author', author: req.body, errors: errors.array() });
+            return;
+        }
+        else {
+            // Data from form is valid.
+
+            // Create an Author object with escaped and trimmed data.
+            var author = new Author(
+                {
+                    first_name: req.body.first_name,
+                    family_name: req.body.family_name,
+                    date_of_birth: req.body.date_of_birth,
+                    date_of_death: req.body.date_of_death
+                });
+            author.save(function (err) {
+                if (err) { return next(err); }
+                // Successful - redirect to new author record.
+                res.redirect(author.url);
+            });
+        }
+    }
+];
 
 // Display Author delete form on GET.
 exports.author_delete_get = function(req, res) {
