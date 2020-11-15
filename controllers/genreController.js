5bec45e02bf0c1b037619b4a48d1835d155e95f0
JuanG <juangiacosabrk@gmail.com>
2020-11-15 17:47:42 -0300

    Implemented genre update page Added genre update btn on genre detail page

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -119,10 +119,48 @@ exports.genre_delete_post = function(req, res) {
 
 // Display Genre update form on GET.
 exports.genre_update_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: Genre update GET');
+  Genre.findById(req.params.id)
+    .exec(function (err, genre) {
+        if (err) { return next(err); }
+        if (genre==null) { // No results.
+            var err = new Error('Genre not found');
+            err.status = 404;
+            return next(err);
+        }
+        // Successful, so render.
+        res.render('genre_form', { title: 'Update Genre: '+ genre.name, genre: genre });
+    })
 };
 
 // Handle Genre update on POST.
-exports.genre_update_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: Genre update POST');
-};
+exports.genre_update_post = [
+  // Validate and santise the name field.
+  body('name', 'Genre name required').trim().isLength({ min: 1 }).escape(),
+
+  (req, res, next) => {
+    
+    // Extract the validation errors from a request.
+    const errors = validationResult(req);
+
+    // Create a genre object with escaped and trimmed data.
+    var genre = new Genre(
+      { name: req.body.name }
+    );
+
+    if (!errors.isEmpty()) {
+      // There are errors. Render the form again with sanitized values/error messages.
+      res.render('genre_form', { title: 'Update Genre: ' + genre.name, genre: genre, errors: errors.array()});
+      return;
+    }
+    else {
+      // Data from form is valid. Update the record.
+      genre._id = req.params.id //This is required, or a new ID will be assigned!
+
+      Genre.findByIdAndUpdate(req.params.id, genre, {}, function (err,theGenre) {
+        if (err) { return next(err); }
+        // Successful - redirect to book detail page.
+        res.redirect(theGenre.url);
+      });
+    }
+  }
+];
